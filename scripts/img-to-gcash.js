a050897089e5b6d144299f3854ecdfd7f08883f2
Wilson Anciro <konekred@gmail.com>
2023-09-01 12:48:12 +0800

    up

---
+++ scripts/img-to-gcash.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+
+const fs = require('fs')
+const rootPath = process.cwd()
+
+const { runShell } = require('../lib')
+const { time } = require('../lib/time')
+const { extractType, extractTransaction } = require('../lib/gcash-extractor')
+
+const getTransaction = async () => {
+  const timestamp = Math.floor(new Date().getTime() / 1000)
+
+  const path = '/sdcard/Download'
+  const filename = `img_${timestamp}.png`
+  const outputImg = `${rootPath}/tmp/output.png`
+  const outputTxt = `${rootPath}/tmp/output`
+
+  await runShell(`adb shell screencap -p ${path}/${filename}`)
+  await runShell(`adb pull ${path}/${filename} ${outputImg}`)
+  await runShell(`adb shell rm ${path}/${filename}`)
+  await runShell(`tesseract ${outputImg} ${outputTxt}`)
+
+  const content = fs.readFileSync(`${outputTxt}.txt`)
+  const cleanContent = content.toString().replace(/\n/g, ' ').replace(/ +/g, ' ')
+
+  const transactionType = extractType(cleanContent)
+
+  if (transactionType) {
+    return extractTransaction(cleanContent, transactionType)
+  }
+
+  return null
+}
+
+getTransaction().then(transaction => {
+  if (transaction) {
+    const filename = `${rootPath}/transactions/${process.env.DATE}-${process.env.ACTIVE_SIM}--${process.env.DUTY}.json`
+
+    if (!fs.existsSync(filename)) {
+      fs.writeFileSync(filename, '[]', 'utf8')
+    }
+
+    const content = fs.readFileSync(filename)
+    const transactions = JSON.parse(content.toString())
+    transaction.duty = process.env.DUTY
+    transaction.num = transactions.length + 1
+    transaction.id = ""
+    transaction.note = ""
+
+    transactions.unshift(transaction)
+
+    fs.writeFileSync(filename, JSON.stringify(transactions, null, 2), 'utf8')
+    console.log('transaction saved.')
+  }
+})
---
+++ test.js
@@ -0,0 +1,34 @@
+
+const fs = require('fs')
+
+const { runShell } = require('./lib')
+const { extractType, extractTransaction } = require('./lib/gcash-extractor')
+
+const getTransaction = async () => {
+  const timestamp = Math.floor(new Date().getTime() / 1000)
+
+  const path = '/sdcard/Download'
+  const filename = `img_${timestamp}.png`
+  const outputImg = './tmp/output.png'
+  const outputTxt = './tmp/output'
+
+  await runShell(`adb shell screencap -p ${path}/${filename}`)
+  await runShell(`adb pull ${path}/${filename} ${outputImg}`)
+  await runShell(`adb shell rm ${path}/${filename}`)
+  await runShell(`tesseract ${outputImg} ${outputTxt}`)
+
+  const content = fs.readFileSync(`${outputTxt}.txt`)
+  const cleanContent = content.toString().replace(/\n/g, ' ').replace(/ +/g, ' ')
+
+  const transactionType = extractType(cleanContent)
+
+  if (transactionType) {
+    return extractTransaction(cleanContent, transactionType)
+  }
+
+  return null
+}
+
+getTransaction().then(transaction => {
+  console.log(transaction)
+})
