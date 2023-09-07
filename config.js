d63b6b387da8566e811c74e44f9a08043d9b59bd
Wilson Anciro <konekred@gmail.com>
2023-09-08 04:48:47 +0800

    up

--- config.js
+++ config.js
@@ -1,189 +1,228 @@
 const config = {
   '': {
     date: '',
-    counter: '',
     phone: '',
     sim: '',
     mobile: ''
   },
+  '048': {
+    date: '',
+    phone: '16',
+    sim: '29',
+    mobile: '0945-487-3097'
+  },
+  '046': {
+    date: '',
+    phone: '14',
+    sim: '53',
+    mobile: '0945-584-2792'
+  },
+  '045': {
+    date: '',
+    phone: '13',
+    sim: '60',
+    mobile: '0977-463-3167'
+  },
+  '044': {
+    date: '',
+    phone: '12',
+    sim: '73',
+    mobile: '0967-433-7521'
+  },
+  '042': {
+    date: '',
+    phone: '10',
+    sim: '74',
+    mobile: '0977-093-6297'
+  },
+  '039': {
+    date: '',
+    phone: '07',
+    sim: '66',
+    mobile: '0977-463-3114'
+  },
+  '037': {
+    date: '',
+    phone: '05',
+    sim: '70',
+    mobile: '0950-611-3644'
+  },
+  '036': {
+    date: '',
+    phone: '04',
+    sim: '79',
+    mobile: '0945-584-2778'
+  },
+  '035': {
+    date: '',
+    phone: '03',
+    sim: '15',
+    mobile: '0919-413-8875'
+  },
+  '033': {
+    date: '',
+    phone: '01',
+    sim: '20',
+    mobile: '0919-413-8369'
+  },
+  '032': {
+    date: '',
+    phone: '16',
+    sim: '28',
+    mobile: '0977-464-3571'
+  },
   '017': {
     date: 'SEP.07',
-    counter: '017',
     phone: '01',
     sim: '41',
     mobile: '0945-584-2667'
   },
   '030': {
     date: 'SEP.08',
-    counter: '030',
     phone: '14',
     sim: '68',
     mobile: '0977-463-3408'
   },
   '029': {
     date: 'SEP.08',
-    counter: '029',
     phone: '13',
     sim: '58',
     mobile: '0945-584-2759'
   },
   '027': {
     date: 'SEP.08',
-    counter: '027',
     phone: '11',
     sim: '42',
     mobile: '0945-487-3100'
   },
   '026': {
     date: 'SEP.07',
-    counter: '026',
     phone: '10',
     sim: '10',
     mobile: '0970-824-7411'
   },
   '025': {
     date: 'SEP.07',
-    counter: '025',
     phone: '09',
     sim: '48',
     mobile: '0951-118-4380'
   },
   '023': {
     date: 'SEP.07',
-    counter: '023',
     phone: '07',
     sim: '64',
     mobile: '0945-584-2782'
   },
   '022': {
     date: 'SEP.07',
-    counter: '022',
     phone: '06',
     sim: '18',
     mobile: '0945-487-1955'
   },
   '021': {
     date: 'SEP.07',
-    counter: '021',
     phone: '05',
     sim: '44',
     mobile: '0951-499-7039'
   },
   '020': {
     date: 'SEP.07',
-    counter: '020',
     phone: '04',
     sim: '72',
     mobile: '0977-093-6299'
   },
   '019': {
     date: 'SEP.07',
-    counter: '019',
     phone: '03',
     sim: '57',
     mobile: '0945-584-2801'
   },
   '018': {
     date: 'SEP.07',
-    counter: '018',
     phone: '02',
     sim: '14',
     mobile: '0956-896-6669'
   },
   '016': {
     date: 'SEP.07',
-    counter: '016',
     phone: '16',
     sim: '27',
     mobile: '0945-487-3101'
   },
   '014': {
     date: 'SEP.07',
-    counter: '014',
     phone: '09',
     sim: '47',
     mobile: '0969-179-1884'
   },
   '012': {
     date: 'SEP.07',
-    counter: '012',
     phone: '06',
     sim: '49',
     mobile: '0977-465-0265'
   },
   '011': {
     date: 'SEP.06',
-    counter: '011',
     phone: '05',
     sim: '43',
     mobile: '0905-503-3688'
   },
   '010': {
     date: 'SEP.06',
-    counter: '010',
     phone: '02',
     sim: '38',
     mobile: '0945-584-2793'
   },
   '009': {
     date: 'SEP.06',
-    counter: '009',
     phone: '01',
     sim: '12',
     mobile: '0992-688-7592'
   },
   '008': {
     date: 'SEP.03',
-    counter: '008',
     phone: '14',
     sim: '69',
     mobile: '0908-130-7434'
   },
   '007': {
     date: 'SEP.03',
-    counter: '007',
     phone: '11',
     sim: '21',
     mobile: '0927-747-5671'
   },
   '006': {
     date: 'SEP.01',
-    counter: '006',
     phone: '03',
     sim: '19',
     mobile: '0977-464-3549'
   },
   '005': {
     date: 'SEP.01',
-    counter: '005',
     phone: '10',
     sim: '24',
     mobile: '0994-941-0110'
   },
   '004': {
     date: 'SEP.01',
-    counter: '004',
     phone: '12',
     sim: '26',
     mobile: '0908-610-2524'
   },
   '003': {
     date: 'SEP.01',
-    counter: '003',
     phone: '04',
     sim: '13',
     mobile: '0977-462-6068'
   },
   '002': {
     date: 'SEP.01',
-    counter: '002',
     phone: '13',
     sim: '56',
     mobile: '0945-584-2800'
   },
   '001': {
     date: 'SEP.01',
-    counter: '001',
     phone: '07',
     sim: '63',
     mobile: '0915-470-4990'
