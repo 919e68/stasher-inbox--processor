da4357838ca28b477dc538999429b79965b406ad
Wilson Anciro <konekred@gmail.com>
2023-09-17 18:45:55 +0800

    up

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
+require('dotenv').config()
+const rootPath = process.cwd()
 
 const fs = require('fs')
 
 // const { runShell } = require('./lib')
-const { extractBalance } = require('./lib/gcash-extractor')
+const { extractType, extractTransaction } = require(`${rootPath}/lib/gcash-extractor`)
 
+const content = `
+Today, 06:31 PM 0&8
 
-console.log(process.argv)
+Express Send Notification
+
+You have received PHP 100.00 of GCash from M**
+CH********R U, 09497862082 w/ MSG: . Your new
+balance is PHP 9688.00. Ref. No. 7012121108038.
+`
+
+const cleanContent = content
+.toString()
+.replace(/\n/g, ' ')
+.replace(/ +/g, ' ')
+
+const transactionType = extractType(cleanContent)
+
+if (
+  transactionType &&
+  cleanContent.toLocaleLowerCase().indexOf('latest') === -1
+) {
+  const transaction = extractTransaction(cleanContent, transactionType)
+  console.log(transaction)
+}
