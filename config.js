05c9a9e8811bfc5497800b0dbffd0685f44a5990
Wilson Anciro <konekred@gmail.com>
2023-10-04 20:43:36 +0800

    up

--- config.js
+++ config.js
@@ -1,51 +1,93 @@
 const config = {
-  524: {
+  531: {
     date: 'OCT.',
     phone: '',
     sim: '',
     wallet: ''
+  },
+  530: {
+    date: 'OCT.',
+    phone: '17',
+    sim: '103',
+    wallet: '0915-646-7877'
+  },
+  529: {
+    date: 'OCT.',
+    phone: '16',
+    sim: '29',
+    wallet: '0945-487-3097'
+  },
+  528: {
+    date: 'OCT.',
+    phone: '14',
+    sim: '69',
+    wallet: '0908-130-7434'
+  },
+  527: {
+    date: 'OCT.',
+    phone: '13',
+    sim: '58',
+    wallet: '0945-584-2759'
+  },
+  526: {
+    date: 'OCT.',
+    phone: '12',
+    sim: '123',
+    wallet: '0915-646-7909'
+  },
+  525: {
+    date: 'OCT.',
+    phone: '11',
+    sim: '128',
+    wallet: '0912-336-0685'
+  },
+  524: {
+    date: 'OCT.',
+    phone: '10',
+    sim: '10',
+    wallet: '0970-824-7411'
   },
   523: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '09',
+    sim: '134',
+    wallet: '0915-646-7726'
   },
   522: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '08',
+    sim: '62',
+    wallet: '0945-584-2798'
   },
   521: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '07',
+    sim: '66',
+    wallet: '0977-463-3114'
   },
   520: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '06',
+    sim: '88',
+    wallet: '0931-099-0774'
   },
   519: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '05',
+    sim: '43',
+    wallet: '0905-503-3688'
   },
   518: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '04',
+    sim: '13',
+    wallet: '0977-462-6068'
   },
   517: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '03',
+    sim: '82',
+    wallet: '0908-418-6935'
   },
   516: {
     date: 'OCT.',
@@ -55,15 +97,15 @@ const config = {
   },
   515: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '01',
+    sim: '105',
+    wallet: '0945-584-2802'
   },
   514: {
     date: 'OCT.',
-    phone: '',
-    sim: '',
-    wallet: ''
+    phone: '00',
+    sim: '92',
+    wallet: '0960-412-6166'
   },
   513: {
     date: 'OCT.03',
